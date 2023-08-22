df38826afb870309b635e7f61cfebf170b9c3d47
clickers99 <arfansyahirham@gmail.com>
2023-08-22 21:27:01 +0700

    merapikan line menjadi teratur

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -9,20 +9,20 @@ const expectedPassword = 'superpassword';
 
 /* Comment : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
-      const email = inputEmailElement.value;
-      const password = inputPasswordElement.value;
+  /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
 
-    /* Comment Memastikan bahwa nilai email dan password sesuai dengan email yang tersimpan. */
-    if (email == expectedEmail && password == expectedPassword) {
-      
-      /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
-      goToHome();
-    } else {
+  /* Comment Memastikan bahwa nilai email dan password sesuai dengan email yang tersimpan. */
+  if (email == expectedEmail && password == expectedPassword) {
+    
+    /* Comment : Jika sesuai maka program akan berpindah ke halaman home. */
+    goToHome();
+  } else {
 
-      /* Comment : Namun jika tidak sesuai makan akan menampilkan informasi bahwa input salah. */
-      showPopUp();
-    }
+    /* Comment : Namun jika tidak sesuai makan akan menampilkan informasi bahwa input salah. */
+    showPopUp();
+  }
 });
