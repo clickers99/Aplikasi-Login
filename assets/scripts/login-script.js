ccb4ef464b88eb14890421a24b7b7671c1f7cbda
clickers99 <arfansyahirham@gmail.com>
2023-08-22 21:20:32 +0700

    mengubah comment menjadi lebih rapi

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,10 +1,3 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
 /* Comment : Membuat variabel untuk setiap elemen view. */
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
@@ -17,15 +10,10 @@ const expectedPassword = 'superpassword';
 /* Comment : Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-    
+
     /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
       const email = inputEmailElement.value;
       const password = inputPasswordElement.value;
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
 
     /* Comment Memastikan bahwa nilai email dan password sesuai dengan email yang tersimpan. */
     if (email == expectedEmail && password == expectedPassword) {
